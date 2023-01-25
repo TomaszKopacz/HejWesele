bc47f517e0b770902efbb3460c57238cb1d09bf1
Tomasz Kopacz <tomasz.kopacz@onet.pl>
2023-01-25 13:57:55 +0100

    [HWE-7] Home - Couple Lottie animation

--- presentation/features/home/src/main/kotlin/com/hejwesele/home/Home.kt
+++ presentation/features/home/src/main/kotlin/com/hejwesele/home/Home.kt
@@ -1,19 +1,29 @@
 package com.hejwesele.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.hejwesele.android.components.TextBodyLarge
+import com.hejwesele.android.theme.Dimension
 
 @Composable
 fun Home(eventId: Int) {
@@ -28,9 +38,21 @@ fun Home(eventId: Int) {
                     .asPaddingValues()
             )
     ) {
-        TextBodyLarge(
-            modifier = Modifier.align(Alignment.Center),
-            text = "Hej Wesele! Id wydarzenia: $eventId"
-        )
+        Column(
+            modifier = Modifier.padding(Dimension.marginLarge).fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            CoupleLottieAnimation()
+            TextBodyLarge(
+                text = "Hej Wesele! Id wydarzenia: $eventId"
+            )
+        }
     }
+}
+
+@Composable
+fun CoupleLottieAnimation() {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.lottie_wedding_couple))
+    LottieAnimation(composition = composition, iterations = LottieConstants.IterateForever, modifier = Modifier.aspectRatio(1f))
 }
--- presentation/ui/theme/src/main/kotlin/com/hejwesele/android/theme/Dimension.kt
+++ presentation/ui/theme/src/main/kotlin/com/hejwesele/android/theme/Dimension.kt
@@ -3,5 +3,8 @@ package com.hejwesele.android.theme
 import androidx.compose.ui.unit.dp
 
 object Dimension {
+    val marginNormal = 16.dp
+    val marginLarge = 24.dp
+
     val iconNormal = 24.dp
 }
