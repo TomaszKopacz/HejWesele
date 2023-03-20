a0b46249b1166d958d64f8c038f9f51bf294e033
Tomasz Kopacz <tomasz.kopacz@onet.pl>
2023-03-20 11:16:41 +0100

    [HWE-38] Refactor - CustomTabs module removed

--- common/customtabs/src/main/kotlin/com/hejwesele/android/customtabs/CustomTabs.kt
+++
@@ -1,5 +0,0 @@
-package com.hejwesele.android.customtabs
-
-interface CustomTabs {
-    fun launch(url: String)
-}
--- common/customtabs/src/main/kotlin/com/hejwesele/android/customtabs/LocalCustomTabs.kt
+++
@@ -1,7 +0,0 @@
-package com.hejwesele.android.customtabs
-
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalCustomTabs = staticCompositionLocalOf<CustomTabs> {
-    error("CompositionLocal LocalCustomTabs not present")
-}
